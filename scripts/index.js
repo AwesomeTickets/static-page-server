8c2d044b51e56903bb762892d12d96591acc796d
liveipool <liveipool@liveipool-CW35S.lan>
2016-12-03 23:30:04 +0800

    finish validate of signup

--- scripts/index.js
+++ scripts/index.js
@@ -3,58 +3,6 @@ $(document).ready(function() {
 		window.location = "layouts/signup.html";
 	});
 
-	// EventUtil={
-		
-	//    addHandler:function(element,type,handler){ //添加事件
-	//       if(element.addEventListener){ 
-	//          element.addEventListener(type,handler,false);  //使用DOM2级方法添加事件
-	//       }else if(element.attachEvent){                    //使用IE方法添加事件
-	//          element.attachEvent("on"+type,handler);
-	//       }else{
-	//          element["on"+type]=handler;          //使用DOM0级方法添加事件
-	//       }
-	//    },  
-
-	//    removeHandler:function(element,type,handler){  //取消事件
-	//       if(element.removeEventListener){
-	//          element.removeEventListener(type,handler,false);
-	//       }else if(element.detachEvent){
-	//          element.detachEvent("on"+type,handler);
-	//       }else{
-	//          element["on"+type]=null;
-	//       }
-	//    },
-
-	//    getEvent:function(event){  //使用这个方法跨浏览器取得event对象
-	//       return event?event:window.event;
-	//    },
-		
-	//    getTarget:function(event){  //返回事件的实际目标
-	//       return event.target||event.srcElement;
-	//    },
-		
-	//    preventDefault:function(event){   //阻止事件的默认行为
-	//       if(event.preventDefault){
-	//          event.preventDefault(); 
-	//       }else{
-	//          event.returnValue=false;
-	//       }
-	//    }
-			
-	// };
-
-	// EventUtil.addHandler($("#signin"), "submit", function(event) {
-	// 	//获取事件对象
-	// 	event = EventUtil.getEvent(event);
-	// 	EventUtil.preventDefault(event);
-	// });
-	// $("#signin").click(function() {
-	// 	console.log("hha");
-	// 	$("#signin").submit();
-	// 	// var form = document.getElementById("signin");
-	// 	console.log("form: ", $("#signin"));
-	// 	// form.submit();
-	// })
 	$( "#signin" ).click(function( event ) {
 		var username = $("#inputUsernameSignin")[0].value;
 		var password = $("#inputPasswordSignin")[0].value;
--- scripts/validator.js
+++ scripts/validator.js
@@ -2,57 +2,57 @@ validator = { //这是一个什么变量:一个对象 类似于json 里面装内
   form: {
     username: {
       status: false,
-      errorMessage: '6~18位英文字母、数字或下划线，必须以英文字母开头'
+      errorMessage: '用户名为6~18位英文字母、数字或下划线，必须以英文字母开头'
     }, 
-    sid: {
+    password: {
       status: false,
-      errorMessage: '8位数字，不能以0开头'
-    }, 
+      errorMessage: '密码为~18位英文字母、数字或下划线，必须以英文字母开头'
+    },
+    name: {
+      status: false,
+      errorMessage: '姓名不能为空'
+    },
+    sex: {
+      status: false,
+      errorMessage: '性别不能为空'
+    },
     phone: {
       status: false,
-      errorMessage: '11位数字，不能以0开头'
-    }, 
-    email: {
+      errorMessage: '电话号码不能为空'
+    },
+    address: {
       status: false,
-      errorMessage: '请输入合法邮箱'
+      errorMessage: '地址不能为空'
     }
   }, 
 
-  isUsernameValid: function (username){
+  isUsernameValid: function (username) {
     return this.form.username.status = /^[a-zA-Z][a-zA-Z0-9_]{6,18}$/.test(username);
+  },
+
+  isPasswordValid: function (password) {
+    return this.form.password.status = /^[a-zA-Z][a-zA-Z0-9_]{6,18}$/.test(password);
+  },
+
+  isNameValid: function (name) {
+    return this.form.name.status = name != ""; 
+  },
+
+  isSexValid: function (sex) {
+    return this.form.sex.status = sex != ""; 
+  },
+
+  isPhoneValid: function (phone) {
+    return this.form.phone.status = phone != ""; 
+  },
+
+  isAddressValid: function (address) {
+    return this.form.address.status = address != ""; 
   }
-
-  // isSidValid: function (sid){
-  //   return this.form.sid.status = /^[1-9]\d{7}$/.test(sid);
-  // },
-
-  // isPhoneValid: function (phone){
-  //   return this.form.phone.status = /^[1-9]\d{10}$/.test(phone);
-  // },
-
-  // isEmailValid: function (email){
-  //   return this.form.email.status = /^[a-zA-Z_\-]+@([a-zA-Z_\-]+\.)+[a-zA-Z]{2,4}$/.test(email);
-  // },
-
-  // isFieldValid: function(fieldname, value){
-  //   var CapFieldname = fieldname[0].toUpperCase() + fieldname.slice(1, fieldname.length);
-  //   return this["is" + CapFieldname + 'Valid'](value);
-  // },  //动态地来检验四种数据是否合法
-
   // isFormValid: function(){
   //   return this.form.username.status && this.form.sid.status && this.form.phone.status && this.form.email.status;
   // }, //检查整个表单是否合法
 
-  // getErrorMessage: function(fieldname){
-  //   return this.form[fieldname].errorMessage;
-  // },
-
-  // isAttrValueUnique: function(registry, user, attr){
-  //   for (var key in registry) {
-  //     if (registry.hasOwnProperty(key) && registry[key][attr] == user[attr]) return false;
-  //   }
-  //   return true;
-  // }
 }
 
 // if (typeof module == 'object') { // 服务端共享
