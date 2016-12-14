041f5c3507d87f10898ee577cc23d3fd8ab028c7
liveipool <liveipool@liveipool-CW35S.lan>
2016-12-15 01:11:54 +0800

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var inject = require('gulp-inject');
-var del = require('del');
+var del = require('del');  //用来删除文件的
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
 
 gulp.task('webserver', function() {
 	connect.server({
-		root: 'tmp',
+		root: 'tmp',  //程序入口处，自动为这个文件夹下的Index.html，具体为啥是这个文件可能是gulp默认的，应该可以改
 		livereload: true
 	});
 });
@@ -20,10 +20,10 @@ gulp.task('sass', function() {
 	gulp.src('style.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('styles'))
-		.pipe(connect.reload());
+		.pipe(connect.reload());  //用来实现更改自动刷新
 });
 
-gulp.task('css', ['sass'], function() {
+gulp.task('css', ['sass'], function() {   //['sass']指sass这个task完成之后再执行css这个task。
 	gulp.src('styles/*.css')
 		.pipe(gulp.dest('tmp/styles'))
 		.pipe(connect.reload());
@@ -35,6 +35,7 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
+//static这一部分应该有更简单的写法，但要求不多所以暂时这么写也没问题
 gulp.task('static', function() {
 	gulp.src(['static/pictures/*.jpg', 'static/pictures/*.JPG', 'static/pictures/*.png', 'static/pictures/*.PNG', 'static/pictures/*.gif', 'static/pictures/*.jpeg'])
 		.pipe(gulp.dest('tmp/static/pictures'));
