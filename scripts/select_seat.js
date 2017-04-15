c578b165628d31085677866938c4ad7673131fc9
Liveipool <123653079@qq.com>
2017-04-15 22:33:54 +0800

    finish seting unavailable seats, waiting for backend APIs

--- scripts/select_seat.js
+++ scripts/select_seat.js
@@ -14,9 +14,17 @@ $(document).ready(function() {
   // 获取不可用座位信息
   const unavailable = {
     "count": 3,
-    "data": [[5, 3], [5, 4], [6, 3], [6, 4], [6, 5], [6, 6]],
+    // "data": [[1, 1], [2, 1], [3, 4], [3, 5]],
+    "data": [[4, 1], [4, 4], [8, 1], [8, 3]],
   }
   /*模拟数据*/
+
+  /*点击LOGO回到主页开始*/
+  let head_bar_img = document.getElementById('head_bar_img');
+  head_bar_img.onclick = function() {
+    window.location = '../index.html';
+  }
+  /*点击LOGO回到主页结束*/
 
   /*选择座位部分开始*/
 
@@ -37,12 +45,14 @@ $(document).ready(function() {
     let select_seat_seats = document.createElement('div');
     select_seat_seats.className = 'select_seat_seats';
     select_seat_seats.style.width = select_seat_column_count * 28 + 'px'; 
+
     for (let j = 1; j <= select_seat_column_count; j++) {
       let select_seat_seats_item = document.createElement('div');
       select_seat_seats_item.className = 'select_seat_seats_item';
       select_seat_seats_item.id = 'select_seat_' + i + '_' + j;
+
+
       if (select_seat_layout_matrix[i - 1].slice(j - 1, j) == 1) {
-        console.log(select_seat_seats_item.style.backgroundImage);
         select_seat_seats_item.style.backgroundImage = 'url(\'/static/pictures/assets/seats/available.png\')';
       }
       select_seat_seats.appendChild(select_seat_seats_item);
@@ -51,7 +61,22 @@ $(document).ready(function() {
     select_seat_row.appendChild(select_seat_seats);
     select_seat_fragment.appendChild(select_seat_row);
   }
+
   select_seat_dot_line.style.height = 40 * select_seat_row_count + 10 + 'px';
   select_seat.appendChild(select_seat_fragment);
+
+  // 将不可选座位的图标改为红色
+  for (let i = 0; i < unavailable.data.length; i++) {
+    let select_seat_this_row_array = [...select_seat_layout_matrix[unavailable.data[i][0] - 1]],
+      tmpObj = {};
+    for (let j = 0; j < select_seat_this_row_array.length; j++) {
+      if (select_seat_this_row_array[j] == 1) {
+        tmpObj[j] = '1';
+      }
+    }
+    let tmpArr = Object.keys(tmpObj);
+    document.getElementById('select_seat_' + unavailable.data[i][0] + '_' + (parseInt(tmpArr[unavailable.data[i][1] - 1]) + 1)).className += ' select_seat_seats_item_unavailable';
+  }
+
   /*选择座位部分结束*/
 });
